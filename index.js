6cd02af84fd503f01c63b181719d12170f1ecc7b
Jonathan Hao <johnathan79717@gmail.com>
2016-02-16 14:58:29 +0800

    Support Australia, UK, Phillipines, Japan

--- index.js
+++ index.js
@@ -2,15 +2,24 @@ var events = require("sdk/system/events");
 var { Ci,Cu } = require("chrome");
 Cu.import("resource://gre/modules/Services.jsm");
 
+var local_nba = [
+    ["http://nba.udn.com/nba/index?gr=www", "taiwanuw"],
+    ["http://australia.sportingnews.com/nba?gr=www", "australiauw"],
+    ["http://www.givemesport.com/nba?gr=www", "ukuw"],
+    ["http://ph.nba.com/?gr=www", "philippinesuw"],
+    ["http://www.nba.co.jp/?gr=www", "japanuw"],
+];
+
 function listener(event) {
     var subject = event.subject;
     var httpChannel = subject.QueryInterface(Ci.nsIHttpChannel);
     var url = subject.URI.spec;
-    var local_nba = "http://nba.udn.com/nba/index?gr=www";
 
-    if(url === local_nba) {
-        var global_nba = "http://www.nba.com/gr/reflectIntl.html?gr=taiwanuw&page=www&dur=ses";
-        subject.redirectTo(Services.io.newURI(global_nba, null, null));
+    for (var i = 0; i < local_nba.length; i++) {
+        if(local_nba[i][0] === url) {
+            var global_nba = "http://www.nba.com/gr/reflectIntl.html?page=www&dur=ses&gr=" + local_nba[i][1];
+            subject.redirectTo(Services.io.newURI(global_nba, null, null));
+        }
     }
 }
 events.on("http-on-modify-request", listener);
