7118a12e13aec5000299154aa3fa47da0579f21a
Jonathan Hao <johnathan79717@gmail.com>
2016-02-18 22:54:44 +0800

    Store redirects in json

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
+var self = require("sdk/self")
 var events = require("sdk/system/events");
 var { Ci,Cu } = require("chrome");
 Cu.import("resource://gre/modules/Services.jsm");
 
-var local_nba = [
-    ["http://nba.udn.com/nba/index?gr=www", "taiwanuw"],
-    ["http://australia.sportingnews.com/nba?gr=www", "australiauw"],
-    ["http://www.givemesport.com/nba?gr=www", "ukuw"],
-    ["http://ph.nba.com/?gr=www", "philippinesuw"],
-    ["http://www.nba.co.jp/?gr=www", "japanuw"],
-    ["http://www.gazzetta.it/Nba/?gr=www", "italyuw"],
-    ["http://www.lequipe.fr/Basket/Nba/?gr=www", "franceuw"],
-    ["http://www.spox.com/de/sport/ussport/nba/index.html?gr=www", "germanyuw"],
-];
+var redirects = JSON.parse(self.data.load("redirects.json"))
+
+var nba_prefix = "http://www.nba.com/gr/reflectIntl.html?page=www&dur=ses&gr=";
 
 function listener(event) {
     var subject = event.subject;
     var httpChannel = subject.QueryInterface(Ci.nsIHttpChannel);
     var url = subject.URI.spec;
 
-    for (var i = 0; i < local_nba.length; i++) {
-        if(local_nba[i][0] === url) {
-            var global_nba = "http://www.nba.com/gr/reflectIntl.html?page=www&dur=ses&gr=" + local_nba[i][1];
-            subject.redirectTo(Services.io.newURI(global_nba, null, null));
+    for (var redirect of redirects) {
+        if(redirect.url === url) {
+            subject.redirectTo(Services.io.newURI(nba_prefix + redirect.cookie, null, null));
             break;
         }
     }
