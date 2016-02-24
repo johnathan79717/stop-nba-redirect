dd7736f0d2ea1c693831eefd504516bc902f76d0
Jonathan Hao <johnathan79717@gmail.com>
2016-02-25 00:36:24 +0800

    Set cookie

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#Stop NBA Redirect V0.0.3
+#Stop NBA Redirect V1.0
 It is hosted here: https://addons.mozilla.org/en-US/firefox/addon/stop-nba-redirect/ 
 
 With this add-on, when you navigate to http://nba.com, you will not be redirect to your countries' local sites.
--- index.js
+++ index.js
@@ -2,25 +2,52 @@ var self = require("sdk/self")
 var events = require("sdk/system/events");
 var { Ci,Cu } = require("chrome");
 Cu.import("resource://gre/modules/Services.jsm");
+var Request = require("sdk/request").Request;
 
-var redirects = JSON.parse(self.data.load("redirects.json"))
+var urlToCookie = JSON.parse(self.data.load("urlToCookie.json"))
 
-var nba_prefix = "http://www.nba.com/gr/reflectIntl.html?page=www&dur=ses&gr=";
+function setCookieByUrl(url) {
+    var cookieValue = urlToCookie[url];
+    if (cookieValue === undefined) {
+        return false;
+    } else {
+        Services.cookies.add(
+                ".nba.com", // host
+                "/", // path
+                "gr", // key
+                cookieValue, // value
+                false, // https only
+                false, // http only
+                false, // session
+                new Date().getTime() / 1000 + 86400 * 365 * 100); // 100 years
+        return true;
+    }
+}
 
 function listener(event) {
     var subject = event.subject;
     var httpChannel = subject.QueryInterface(Ci.nsIHttpChannel);
     var url = subject.URI.spec;
-
-    for (var redirect of redirects) {
-        if(redirect.url === url) {
-            subject.redirectTo(Services.io.newURI(nba_prefix + redirect.cookie, null, null));
-            break;
-        }
+    if (setCookieByUrl(url)) {
+        subject.redirectTo(Services.io.newURI("http://www.nba.com", null, null));
+        removeListener();
     }
 }
+
+function removeListener() {
+    events.off("http-on-modify-request", listener);
+};
+
 events.on("http-on-modify-request", listener);
 
-exports.onUnload = function() {
-    events.removeListener("http-on-modify-request", listener);
-}
+exports.onUnload = removeListener;
+
+// we don't want the listener to exists for too long,
+// so we intentionally request nba.com to see where we are redirected to
+Request({
+    url: "http://www.nba.com?no-cache",
+    onComplete: function(response) {
+        setCookieByUrl(response.url);
+        removeListener();
+    }
+}).get();
